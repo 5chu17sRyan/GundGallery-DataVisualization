149b8226b0074ce0bc6546701a1942a2dbb596af
Wang ZF <wang1@kenyon.edu>
2020-03-06 10:52:12 -0600

    update slider info

--- shinyapp.R
+++ shinyapp.R
@@ -1,7 +1,10 @@
 install.packages("shiny")
 library(shiny)
 ui <- fluidPage(
-  sliderInput(inputId = "num", label = "# of trees", value = 50, min = 1, max = 108)
+  sliderInput(inputId = "numTree", label = "# of trees", value = 50, min = 1, max = 108)
+  sliderInput(inputId = "DBH", label = "Diameter at breast height (ft)", min = 0.2, max = 5, step = 0.01)
 )
-server <- function(input, output){}
+server <- function(input, output){
+  output
+}
 shinyApp(ui = ui, server = server)
