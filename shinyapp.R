375e94676f91175daeec8f180cd483565b88496d
Ryan Schultz <ryanschultz2018@yahoo.com>
2020-04-07 10:13:30 -0400

    UI - Added title and description. Enlarged output. Re-aligned sliders.

--- shinyapp.R
+++ shinyapp.R
@@ -14,27 +14,36 @@ health_score <- function(benefit){
 
 #ui-----------
 ui <- fluidPage(
+  
+  titlePanel(title=div(img(src="http://cliparts.co/cliparts/dc9/KRR/dc9KRRLEi.png", height = 70), "Gambier Tree Simulator"), windowTitle = "Gambier Tree Simulator "), 
+  tags$h4("This simulation is based on tree data gathered on Kenyon College grounds. You can change the number of trees and the average size of the trees to see the impact removing trees versus letting them grow has on the ecosystem. The display on the right shows a score for the benefit these trees have to the ecosystem"),
+  
+
   fluidRow(
-    column(6, 
-      sliderInput(inputId = "numTree",
-                 label = "# of trees",
-                 value = 50,
-                 min = 1,
-                 max = 108),
-      sliderInput(inputId = "DBH",
-                 label = "Diameter at breast height (ft)",
-                 value = 2,
-                 min = 0.2,
-                 max = 5,
-                 step = 0.01),
+    column( 5,
+            sliderInput(inputId = "numTree",
+                        label = "Number of Trees",
+                        value = 50,
+                        min = 1,
+                        max = 108,
+                        width = '100%'),
+            sliderInput(inputId = "DBH",
+                        label = "Diameter at breast height (ft)",
+                        value = 2.5,
+                        min = 0.2,
+                        max = 5,
+                        step = 0.01,
+                        width = '100%'),
+            style = "padding-top:50px"
+              
     ),
-    column(6,
+    column(7,
       #textOutput("stats"),
        
       #output donut
       mainPanel(
-        plotOutput(outputId = "donut")
-      )       
+        plotOutput(outputId = "donut", width = '150%', height = 600)
+      )
     )
   )
 )
