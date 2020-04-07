f3214cef470cd21f73724b03f6f3068f2214c5ed
Ryan Schultz <ryanschultz2018@yahoo.com>
2020-04-07 08:41:56 -0400

    Added suggestion about avoiding calculating benefit twice.

--- shinyapp.R
+++ shinyapp.R
@@ -22,7 +22,7 @@ ui <- fluidPage(
               step = 0.01),
 
   textOutput("stats"),
-  
+
   #output donut
   mainPanel(
     plotOutput(outputId = "donut")
@@ -38,21 +38,22 @@ server <- function(input, output){
     paste(
       "The system's health score is: ", health_score(benefit)
     )})
-  
+
   #donut plot--------------
-  
+
   #create dataframe
   donut_data <- data.frame(
     type = c("Unfilled", "Filled"),
     value = c(40, 60) #random numbers that are changed in render
   )
-  
+
   #Make donut chart
   draw_plot <- function(donut_data){
-    
+
     #define benefit again
+    //If we initially calculated benefit outside of the renderText function we could avoid calculating it twice
     benefit <- input$numTree * benefit_from_CBH(pi * input$DBH)
-    
+
     #make bar chart into pie chart
     donut_plot <- ggplot(donut_data, aes(x = 2, y = value, fill = type)) +
       geom_bar(size = 1, color = "transparent", stat = "identity")+
@@ -60,14 +61,14 @@ server <- function(input, output){
       coord_polar("y", start = 0)+
       xlim(-4, 2.5) +   #donut thickness
       annotate(geom = 'text', x = -4, y =100, color="dark green",size=20, label=health_score(benefit)) #make text
-      
+
     #make pie chart into donut chart
     pie_chart <- donut_plot + coord_polar("y", start = 0)
-    pie_chart + 
+    pie_chart +
       theme(legend.position = "none") + #no legend
       scale_fill_manual(values = c("dark green", "white"))
   }
-  
+
   #render donut
   output$donut <- renderPlot({
     benefit <- input$numTree * benefit_from_CBH(pi * input$DBH)
@@ -75,7 +76,7 @@ server <- function(input, output){
     donut_data$value[1] <- 100-health_score(benefit) #Unfilled
     draw_plot(donut_data)
   })
-  
+
 }
 
 shinyApp(ui = ui, server = server)
